e6d1b0d8b1a0b3a67b9824388b0539325e1ea0ed
Rafa <jchavarria@soin.co.cr>
2021-03-03 11:33:13 -0600

    added limit param to scheduler cron

--- src/libs/request-scheduler.js
+++ src/libs/request-scheduler.js
@@ -12,9 +12,9 @@ class requestScheduler {
         })
     }
 
-    async start() {
-        const where = { retries: { $gt: sequelize.col('attempts') } }
-        const data = await this.model.findAll({ where })
+    async start(limit = 10) {
+        const where = { retries: { $gt: sequelize.col('attempts') }, status: -1 }
+        const data = await this.model.findAll({ where, limit })
         const response = toObject(data)
         if(response.lenght) await this.execJob(response)
     }
