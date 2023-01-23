a077c53b92bf416937cafdcedb30fd70896751b9
esalascsoin <esalasc@soin.co.cr>
2023-01-23 12:37:20 -0600

    Master | Modify Request

--- src/libs/request.js
+++ src/libs/request.js
@@ -13,14 +13,21 @@ const { createRequestOptions } = require("../utils/helpers");
  * @property {string}  params.responseType - Default json
  * @property {object}  params.data - Default null
  * @property {throwError}  params.throwError - Default false
- * @return {array} The data from the URL.
+ * @return {array} The data and options from the URL.
  */
-const Request = async (params) => {
+const Request = async (params, options = false) => {
   try {
     const opt = createRequestOptions(params);
 
     const response = await axios(opt);
     const { data } = response;
+
+    if (options && options.returnAttributes && options.returnAttributes.length > 0) {
+      const responseObj = { data }
+      options.returnAttributes.forEach(attr => responseObj[attr] = attr);
+      return responseObj
+    }
+
     return data;
   } catch (error) {
     //se agrega parametro para responder con erro en caso de que falle
@@ -56,58 +63,4 @@ const Request = async (params) => {
   }
 };
 
-/**
- * Promise function based on HTTP client
- * @async
- * @function Request
- * @param {object} params - Object
- * @property {string}  params.method - Default GET
- * @property {object}  params.headers - Default { content-type: application/json }
- * @property {string}  params.url - Required
- * @property {string}  params.responseType - Default json
- * @property {object}  params.data - Default null
- * @property {throwError}  params.throwError - Default false
- * @return {array} JSON with Data and Headers from the URL.
- */
-const RequestWithHeader = async (params) => {
-  try {
-    const opt = createRequestOptions(params);
-
-    const response = await axios(opt);
-    const { data, headers } = response;
-    return { data, headers };
-  } catch (error) {
-    //se agrega parametro para responder con erro en caso de que falle
-    let data = {};
-
-    // Se añade validación para cuando no se logra conectar al server
-    if (error.code === "ECONNREFUSED") {
-      data = { message: `${error.code} ${error.address}`, status: 500 };
-    }
-    // Se añade validación para internal server error
-    if (
-      error.response &&
-      error.response.statusText === "Internal Server Error"
-    ) {
-      data = {
-        message: error.response.data.message,
-        status: error.response.status,
-        statusCode: error.response.status,
-      };
-    }
-    // Retorna el resto de errores posibles
-    const { response } = error;
-    data = response.data;
-    adapterAxiosError(error);
-    if (params && params.throwError) {
-      const errorData = new Error(data.message, data);
-      errorData.statusCode = data.statusCode;
-      errorData.status = response.status;
-      errorData.errors = data.errors;
-      throw errorData;
-    }
-    return data;
-  }
-};
-
-module.exports = { Request, RequestWithHeader };
+module.exports = Request;
