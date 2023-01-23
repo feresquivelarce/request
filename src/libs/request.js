dc0ed1aa646c89ec342a67837ab802e99a525641
esalascsoin <esalasc@soin.co.cr>
2023-01-23 11:57:03 -0600

    Master | Add RequestWithHeader

--- src/libs/request.js
+++ src/libs/request.js
@@ -56,4 +56,58 @@ const Request = async (params) => {
   }
 };
 
-module.exports = Request;
+/**
+ * Promise function based on HTTP client
+ * @async
+ * @function Request
+ * @param {object} params - Object
+ * @property {string}  params.method - Default GET
+ * @property {object}  params.headers - Default { content-type: application/json }
+ * @property {string}  params.url - Required
+ * @property {string}  params.responseType - Default json
+ * @property {object}  params.data - Default null
+ * @property {throwError}  params.throwError - Default false
+ * @return {array} JSON with Data and Headers from the URL.
+ */
+const RequestWithHeader = async (params) => {
+  try {
+    const opt = createRequestOptions(params);
+
+    const response = await axios(opt);
+    const { data, headers } = response;
+    return { data, headers };
+  } catch (error) {
+    //se agrega parametro para responder con erro en caso de que falle
+    let data = {};
+
+    // Se añade validación para cuando no se logra conectar al server
+    if (error.code === "ECONNREFUSED") {
+      data = { message: `${error.code} ${error.address}`, status: 500 };
+    }
+    // Se añade validación para internal server error
+    if (
+      error.response &&
+      error.response.statusText === "Internal Server Error"
+    ) {
+      data = {
+        message: error.response.data.message,
+        status: error.response.status,
+        statusCode: error.response.status,
+      };
+    }
+    // Retorna el resto de errores posibles
+    const { response } = error;
+    data = response.data;
+    adapterAxiosError(error);
+    if (params && params.throwError) {
+      const errorData = new Error(data.message, data);
+      errorData.statusCode = data.statusCode;
+      errorData.status = response.status;
+      errorData.errors = data.errors;
+      throw errorData;
+    }
+    return data;
+  }
+};
+
+module.exports = { Request, RequestWithHeader };
